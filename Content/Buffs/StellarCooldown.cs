e35caf627c4870bcdf854a6e1c531f09a40949f2
AutumnForHire <106860385+AutumnForHire@users.noreply.github.com>
2022-06-04 02:53:53 -0400

    Add files via upload

---
+++ Content/Buffs/StellarCooldown.cs
@@ -0,0 +1,21 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace StarfallenRace.Content.Buffs
+{
+    public class StellarCooldown : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Stellar Cooldown");
+            Description.SetDefault("Stellar Speed is on cooldown and can't be used right now.");
+            Main.buffNoTimeDisplay[Type] = false;
+            Main.debuff[Type] = true;
+            canBeCleared = false;
+            Main.persistentBuff[Type] = true;
+        }
+    }
+}
+    
+    
---
+++ Content/Buffs/StellarSpeed.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace StarfallenRace.Content.Buffs
+{
+	public class StellarSpeed : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Stellar Speed");
+			Description.SetDefault("You feel as agile as a shooting star");
+			Main.buffNoTimeDisplay[Type] = false;
+			Main.debuff[Type] = false;
+			canBeCleared = false;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.moveSpeed += 0.50f;
+			player.allDamage -= 0.10f;
+			player.jumpSpeedBoost += 0.75f;
+		}
+	}
+}
